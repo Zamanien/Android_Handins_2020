7fd4a318a1380c9efc761c3e4a04ac8258c1980b
Zamanien <tari0048@stud.kea.dk>
2020-11-17 14:16:04 +0100

    Note apps can now be saved to Firebase

--- Week44/app/src/main/java/com/example/week44_firecloudnote/MainActivity.java
+++ Week44/app/src/main/java/com/example/week44_firecloudnote/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     //Listview variable - To be referenced later
     ListView listView;
+    Button addNote;
     //Defining an arraylist in the class scope - Not inside a method so it's usage is not limited to that method
     //set to static in order to be accesible from DetailActivity
     static ArrayList<String> myList = new ArrayList<>();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    static ArrayAdapter arrayAdapter;
 
 
     @Override
@@ -32,23 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
         //gets the listview from the ID given in the layout res file
         listView = findViewById(R.id.listView);
-
-
-        //Dummy data
-        myList.add("Item1");
-        myList.add("Item2");
-        myList.add("Item3");
-        myList.add("Item4");
-        myList.add("Item5");
-        myList.add("Item6");
-        myList.add("Item7");
+        addNote = findViewById(R.id.addNote);
 
         //ArrayAdapter takes instance of current class, format (simple list) and the arraylist
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myList);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myList);
 
         //sets an adapter to the listview (the arrayadapter we just made)
         listView.setAdapter(arrayAdapter);
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -59,8 +50,32 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("ID", position);
                 //Starts the activity
                 startActivity(intent);
+            }
+        });
 
+        //Deletes the item on long click
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //if id is not less than -1 (empty list)
+                if (id != -1) {
+                    //remove item - the position as paramter
+                    myList.remove(position);
+                    //shows the data change in the listview
+                    arrayAdapter.notifyDataSetChanged();
+                    //Toast with message
+                    Toast.makeText(getApplicationContext(), "Item deleted", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
+        //back button takes the user to empty note through DetailActivty
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                startActivity(intent);
 
             }
         });
